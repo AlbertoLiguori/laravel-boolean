d50e1e00a1406972f6be8704cf1fa6cce79bf376
Alberto Liguori <a1liguorialberto@gmail.com>
2021-01-28 16:07:59 +0100

    adds detail page for dischi & utenti

--- resources/views/disco.blade.php
+++ resources/views/disco.blade.php
@@ -2,6 +2,10 @@
 
 @section('section-2')
 
-   @dd($disco)
-   
+    <img src=" {{ $disco['poster'] }} " alt="">
+    <h3>{{ $disco['title'] }} </h3>
+    <span class="author"> {{ $disco['author'] }} </span>
+    <span class="year"> {{ $disco['year'] }} </span>
+    <a href="/dischi/{{$disco['id']}}">vedi dettaglio</a>
+
 @endsection
--- resources/views/utente.blade.php
+++ resources/views/utente.blade.php
@@ -2,6 +2,12 @@
 
 @section('section-2')
 
-  @dd($utente)
+<img class="user-img" src=" {{ $utente['image'] }} " alt="">
+<h5>{{ $utente['first_name'] }} </h5>
+<h5>{{ $utente['last_name'] }} </h5>
+
+<h6 class="author"> {{ $utente['email'] }} </h6>
+<span class="year"> {{ $utente['gender'] }} </span>
+<a href="/utenti/{{$utente['id']}}">vedi dettaglio</a>
 
 @endsection
